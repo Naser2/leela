e972622f2532e9fe74ac1fa16bb7f83171b9711f
Naser2 <nassersanou23@gmail.com>
2023-05-08 00:03:42 +0200

    Contact Page Done [2]

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -18,18 +18,18 @@ export default function contact() {
         />
       </Head>
 
-      <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="pt-34 isolate bg-white py-24  px-6 sm:py-32 lg:py-24 lg:px-8 ">
         <div
           className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
           aria-hidden="true"
         >
-          <div
+          {/* <div
             className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#2f5db6] to-[#11193e] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
             }}
-          />
+          /> */}
         </div>
         <div className="mx-auto max-w-2xl text-center">
           <p className="mt-2 text-lg leading-8 text-gray-600">
@@ -41,7 +41,7 @@ export default function contact() {
                 <p className="text-2xl tracking-tight text-black sm:text-3xl lg:mt-10">
                   GET IN TOUCH
                 </p>
-                <p className="text-2 color_15 font_5 font_4 !text-center ">
+                <p className="text-2 color_15 font_5 font_4 py-6 pb-14  !text-center">
                   <address className="contact-address lg:text-2xl">
                     <Link
                       href="mailto:soma@leelanyc.com"
