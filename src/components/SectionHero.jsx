675f03c791a648c876803b5a662aa0b2dd28965d
Naser2 <nassersanou23@gmail.com>
2023-05-07 07:15:41 +0200

    Landing Photos replaced and Sitle title Update

--- src/components/SectionHero.jsx
+++ src/components/SectionHero.jsx
@@ -1,4 +1,5 @@
 import leelaMain from '@/images/leela-main-img.jpeg'
+import leelaMainTemporary1 from '@/images/leelaMainTemporary-1.png'
 import project1 from '@/images/projects-1.png'
 import project2 from '@/images/projects-2.png'
 import project3 from '@/images/projects-3.png'
@@ -9,7 +10,7 @@ import projectXl2 from '@/images/projects-xl-1.png'
 import featuredProject from '@/images/leela-hero-img-3.png'
 export const SectionHero = () => {
   return (
-    <section id="comp-jwuch32x" className=" -mt-20 lg:mt-24 ">
+    <section id="comp-jwuch32x" className=" -mt-20 lg:mt-24  lg:py-10 ">
       <div
         id="bgLayers_comp-jwuch32x"
         data-hook="bgLayers"
@@ -29,12 +30,12 @@ export const SectionHero = () => {
             // className="MW5IWV"
           ></div>
           <div className="mx-6 lg:mx-0">
-            <div>
+            <div className="-pt-34">
               <div
                 id="SITE-TITLE"
-                className="mt-4 max-[600px]:inline-flex  lg:mt-10  lg:-mt-0 "
+                className=" -mt-36 px-[6%] py-[27%] min-[600px]:hidden  "
               >
-                <div className="BaOVQ8 tz5f0K comp-jwucjvdm wixui-rich-text -mt-4 mb-4 md:-mt-0  md:mb-4">
+                {/* <div className="BaOVQ8 tz5f0K comp-jwucjvdm wixui-rich-text -mt-4 mb-4 md:-mt-0  md:mb-4">
                   <div class="col-span-4 col-start-1 w-full max-w-2xl items-center justify-center lg:mt-44 lg:hidden xl:-mb-8 xl:flex-none">
                     <div class="relative  h-full md:-mx-8 xl:mx-0 xl:aspect-auto">
                       <img
@@ -44,69 +45,14 @@ export const SectionHero = () => {
                       />
                     </div>
                   </div>
-                </div>
-                <div className="mt-1 pl-3  lg:mt-10 lg:px-0">
-                  <h1 className="lg:font_4">
-                    <span style={{ fontSize: '72px', lineHeight: '1.05em' }}>
-                      <span className="md:px-0 lg:px-2">
-                        <span className="color_15  site-title">LEELA</span>
-                      </span>
-                    </span>
-                  </h1>
-                  <p className="text-2 font_5 px-2 md:px-2 lg:-mt-0 ">
-                    <span
-                      style={{
-                        fontFamily: 'poppins-extralight, poppins, sans-serif',
-                      }}
-                    >
-                      <span className="color_15 lg:font_5 lg:inline-flex">
-                        <span className="md:px-0">
-                          <h3
-                            style={{
-                              letterSpacing: '0.1em',
-                            }}
-                            className=""
-                          >
-                            CONSULTANCY
-                          </h3>
-                          <h3
-                            style={{
-                              letterSpacing: '0.1em',
-                            }}
-                            className="mt-2 "
-                          >
-                            SERVICE OFFERINGS
-                          </h3>
-                        </span>
-                      </span>{' '}
-                    </span>
-                  </p>
-                </div>
+                </div> */}
+                <SiteTitle />
               </div>
 
               <div className="items-center justify-center gap-y-2 gap-x-4 md:grid md:grid-cols-10 lg:gap-x-8 xl:gap-x-20">
-                {/* <div className="gid relative grid-cols-1 gap-x-2 "> */}
-
-                <div class="col-span-4 col-start-1 w-full max-w-2xl items-center justify-center lg:mt-14 lg:pb-14  xl:-mb-8 xl:flex-none">
-                  <div class="hidden h-full  md:-mx-8 lg:flex xl:mx-0 xl:aspect-auto">
-                    <img
-                      className="h-full flex-auto rounded-t-2xl bg-white"
-                      src={`${featuredProject.src}`}
-                      alt=""
-                    />
-                  </div>
+                <div id="DESKTOP-SITE-TITLE" className="max-[600px]:hidden">
+                  <SiteTitle />
                 </div>
-
-                {/* <div class="mt-14 h-full w-full max-w-2xl xl:-mb-8 xl:flex-none">
-                  <div class="relative aspect-[1/2] h-full xl:mx-0 xl:aspect-auto">
-                    <img
-                      className="aspect-[5/5] w-full bg-gray-800 object-cover shadow-2xl"
-                      src={`${featuredProject.src}`}
-                      alt=""
-                    />
-                  </div>
-                </div> */}
-                {/* </div> */}
                 <div
                   className="MazNVa comp-l94l2olh wixui-image col-start-5 col-end-11"
                   title="leela-main-img"
@@ -116,7 +62,11 @@ export const SectionHero = () => {
                       classNameName="HlRz5e BI8PVQ"
                       data-src="https://static.wixstatic.com/media/0b26de_437bf79c97c946229654539a5e2eeaa1~mv2.jpeg/v1/crop/x_260,y_0,w_838,h_846/fill/w_838,h_846,al_c,q_85,enc_auto/0b26de_437bf79c97c946229654539a5e2eeaa1~mv2.jpeg"
                     >
-                      <img src={leelaMain.src} alt="" fetchpriority="high" />
+                      <img
+                        src={leelaMainTemporary1.src}
+                        alt=""
+                        fetchpriority="high"
+                      />
                     </wow-image>
                   </div>
                 </div>
@@ -164,3 +114,43 @@ export const SectionHero = () => {
     </div>
   </div>
 </div>
+
+const SiteTitle = () => {
+  return (
+    <div className=" pl-3  lg:mt-10 lg:px-0">
+      <h1 className="lg:font_4">
+        <span style={{ fontSize: '36px', lineHeight: '1.05em' }}>
+          <span className="md:px-0 lg:px-2">
+            <span className="color_15  site-title">MARKETING </span>
+          </span>
+        </span>
+      </h1>
+      <p className="text-2 font_4 -mt-4  lg:-mt-4 lg:-mt-0 ">
+        <span
+          style={{
+            fontFamily: 'poppins-extralight, poppins, sans-serif',
+          }}
+        >
+          <span className="color_15 lg:font_5 lg:inline-flex">
+            <span className="md:px-0">
+              <span style={{ fontSize: '36px', lineHeight: '1.05em' }}>
+                <span className="md:px-0 lg:px-2">
+                  <span className="color_15  site-title">CONSULTANCY</span>
+                  {/* <span className="color_15  site-title">Service</span> */}
+                </span>
+              </span>
+              <h3
+                style={{
+                  letterSpacing: '0.1em',
+                }}
+                className="mt-2 md:px-2 "
+              >
+                Branding & Product Strategy. Digital Marketing Development.
+              </h3>
+            </span>
+          </span>{' '}
+        </span>
+      </p>
+    </div>
+  )
+}
