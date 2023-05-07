dfd72f95de1c210420e167ae8121b0fb1d5cbf14
Naser2 <nassersanou23@gmail.com>
2023-05-07 08:46:54 +0200

    About Mobile Done

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -22,14 +22,7 @@ import clsx from 'clsx'
 export default function AboutPage() {
   return (
     <>
-      <div class="hero-content -mt-10 bg-gray-50 lg:pt-24 ">
-        <div class="mx-auto max-w-3xl text-center">
-          {/* <h2 class="text-base font-semibold leading-7 text-blue-400 ">About</h2> */}
-        </div>
-        {/* <h1 class="mt-2 px-6 text-left text-center text-4xl font-bold tracking-tight text-black sm:text-6xl  lg:mt-10  lg:pb-10 ">
-        THE FOUNDER.
-      </h1> */}
-
+      <div class="hero-content -mt-10 lg:pt-16 ">
         <div class="flex  flex-col bg-gray-50 md:grid md:grid-cols-1 xl:px-14 min-[1400px]:grid-cols-2 ">
           <div class="row bg-white">
             <div className="lg:px--12">
@@ -56,7 +49,6 @@ export default function AboutPage() {
                 <span className="sr-only font-extrabold md:not-sr-only md:py-3">
                   Soma Lisa Choudhury
                 </span>
-
                 <div class="lg:-mt-10px group relative mt-[1.23rem] mt-2 flex flex-none flex-col items-start border-transparent bg-sky-50/40 pb-2 text-left hover:border-t-2 md:pb-0 lg:w-[25vw] lg:border-t lg:border-sky-400 lg:hover:border-sky-400 ">
                   <div className="mt-4 flex inline-flex">
                     <h2 class="member-jobtitle -mt-2 px-2 py-1.5  text-2xl text-blue-500  md:order-first  md:bg-sky-50/50 md:px-0  md:px-2  ">
@@ -79,17 +71,12 @@ export default function AboutPage() {
                   </div>
 
                   <p className="mt-0 mb-4 bg-sky-50/30 px-2  text-sm font-semibold leading-6 text-slate-800/80 text-slate-700 md:block lg:mt-2 lg:text-lg">
-                    <h2 class="text-2 font_5 color_15 -mt-2 py-1.5  text-2xl text-slate-600 md:order-first  md:bg-sky-50/50 md:px-2 md:px-3 lg:-mt-0 lg:-ml-4  lg:bg-sky-200/40 ">
+                    <h2 class="text-2 font_5 color_15 -mt-2 py-1.5  text-2xl text-slate-600 md:order-first  md:bg-sky-50/50 md:px-2 md:px-3 lg:-mt-0 lg:-ml-4  lg:bg-sky-200/60 ">
                       Brand Director & Senior marketing executive.
                     </h2>{' '}
-                    <div
-                      id="REALL SOCIAL SHOW UP TO XL"
-                      className="min-[903px]:hidden"
-                    >
-                      <SocialLinks />
-                    </div>
                   </p>
                 </div>
+
                 {/* 
                 <div className="-mt-24px lg:-mt-10px lg::pb-6 group relative flex flex-none flex-col items-start border-t border-transparent bg-white bg-sky-50/40 pb-2 text-left hover:border-t-2  lg:mt-2 lg:w-[45vw]">
                   <p className="mt-0 mt-2 mb-4 bg-sky-50/10  px-2 text-sm  font-semibold leading-6 text-slate-800/70 text-slate-700 md:block  lg:text-lg">
@@ -107,22 +94,66 @@ export default function AboutPage() {
                     </span>
                   </p>
                 </div> */}
-
-                <h2 className="member-quaulities about-page-personal text-bold !lg:-pb-10 py-0 px-2  lg:mt-2 lg:w-[35vw]">
+                <h2 className="member-quaulities about-page-personal text-bold !lg:-pb-10 -mt-4 py-0 px-2 pb-4  lg:mt-2 lg:w-[35vw]">
                   Positive, Artistic &amp; Worldly
                 </h2>
-                <div className=" my-2: max-w-7xl px-2">
+                <div
+                  id="REALL SOCIAL SHOW UP TO XL"
+                  className="inline-flex lg:mt-4"
+                >
+                  <div className="mt-2 mr-4 max-w-7xl px-2">
+                    <div className="contact max-w-2xl lg:mx-0 lg:max-w-xl">
+                      <p className="2">
+                        <Link
+                          href="tel:+1 (9117)-331-8002 "
+                          className="text-font  text-lg md:text-xl"
+                        >
+                          M: 917.331.8002
+                        </Link>
+                      </p>
+                    </div>
+                  </div>{' '}
+                  <Link
+                    href="mailto:soma@leelanyc.com"
+                    className="mt-2 flex max-h-12 max-w-[8rem] items-center rounded-md bg-white py-[0.4375rem] px-2 pr-2 text-sm font-semibold  shadow md:mt-1 lg:max-w-[10rem] lg:pr-3 xl:mt-2 "
+                  >
+                    <img
+                      class="h-5 w-5 flex-none fill-sky-500 stroke-sky-500 text-sky-500 md:h-6 md:w-6 "
+                      src="https://static.xx.fbcdn.net/rsrc.php/v3/yH/r/7nNmS7JFpjU.png"
+                      alt=""
+                      height="14"
+                      width="14"
+                      fill="fill-sky-500"
+                    ></img>
+                    {/* <span className="px-2  text-slate-800 hover:text-blue-500 lg:mx-2 lg:text-lg">
+                      Message
+                    </span> */}
+                  </Link>
+                </div>
+                <div className="my-2: -mt-2 max-w-7xl px-2">
                   <div className="contact max-w-2xl lg:mx-0 lg:max-w-xl">
                     <p className="lg:py-2">
                       <Link
                         href="mailto:info@wht-wrks.com className="
-                        className="text-lg lowercase  text-blue-500 md:text-xl"
+                        className="text-lg  hover:text-blue-500 md:text-xl"
                       >
-                        inquiries@leelanyc.com
+                        SOMA@LEELANYC.COM
                       </Link>
                     </p>
                   </div>
                 </div>
+                {/* <div className=" max-w-7xl px-2">
+                  <div className="contact max-w-2xl lg:mx-0 lg:max-w-xl">
+                    <p className="2">
+                      <Link
+                        href="mailto:info@wht-wrks.com className="
+                        className="text-font  text-lg md:text-xl"
+                      >
+                        M: 917.331.8002
+                      </Link>
+                    </p>
+                  </div>
+                </div> */}
               </h1>
               <div
                 id="HIDDEN UNTIL XXXL"
@@ -151,11 +182,11 @@ export default function AboutPage() {
                       Soma brings years of experience leading, managing,
                       advising, and cultivating various global renowned brands
                       in fashion, beauty &amp; consumer packaged goods. She
-                      collaborates with WHTWRKS clients to define long-term
-                      visions, goals and strategies. Additionally, Soma provides
-                      strategic guidance and tactical services to our team while
-                      creating marketing plans designed to help acquire and
-                      retain customers for each client.
+                      works with clients to define long-term visions, goals and
+                      strategies. Additionally, Soma provides strategic guidance
+                      and tactical services to our team while creating marketing
+                      plans designed to help acquire and retain customers for
+                      each client.
                       {/* <Gallery /> */}
                       <div
                         id="SOCIAL MEDIA HIDDEN LG & XL "
@@ -174,7 +205,7 @@ export default function AboutPage() {
           </div>
         </div>
       </div>
-      <div
+      {/* <div
         id="gallery-MIDDLE_RANGE"
         className="max-[1400px]:hidden  min-[1400px]:contents min-[1703px]:hidden"
       >
@@ -188,7 +219,7 @@ export default function AboutPage() {
           projects={xxLprojects}
           grid={'grid grid-cols-4 gap-x-4 gap-y-4 py-4'}
         />
-      </div>
+      </div> */}
       <div className="md:flex-block bg-gray-50/10 px-6 md:px-14 md:pt-10  lg:px-20  xl:pr-36 min-[1620px]:hidden ">
         <div class="group relative -mt-px flex flex-none flex-col items-start  border-t border-transparent border-t-slate-400  bg-gray-100/60 px-2 pt-4  text-left md:-ml-4 md:w-[35vw] md:px-4 md:pb-6 md:pt-4 lg:w-auto">
           <h2 class="pb-4 text-2xl  md:order-first md:pb-0 lg:px-2 ">BIO</h2>
@@ -204,11 +235,11 @@ export default function AboutPage() {
             executive with global experience in building, launching, and growing
             businesses, brands, and products. Soma brings years of experience
             leading, managing, advising, and cultivating various global renowned
-            brands in fashion, beauty &amp; consumer packaged goods. She
-            collaborates with WHTWRKS clients to define long-term visions, goals
-            and strategies. Additionally, Soma provides strategic guidance and
-            tactical services to our team while creating marketing plans
-            designed to help acquire and retain customers for each client.
+            brands in fashion, beauty &amp; consumer packaged goods. She works
+            with clients to define long-term visions, goals and strategies.
+            Additionally, Soma provides strategic guidance and tactical services
+            to our team while creating marketing plans designed to help acquire
+            and retain customers for each client.
           </span>
         </p>{' '}
       </div>
